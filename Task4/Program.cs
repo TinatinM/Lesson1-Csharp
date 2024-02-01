31cbdc74ec0ef4864b284d178616aab2b89eaaee
TinatinM <tinatinmr@mail.ru>
2024-02-01 22:50:36 +0300

    add lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 60;
+int b = 17;
+int c = 9;
+int d = 14;
+int e = 0;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
